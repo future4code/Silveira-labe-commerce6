9c0f87dc358de774b1bd6f40f9f757d66419f09e
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-03-30 18:38:29 -0300

    Adicionado componentes Card e ContainerProdutos

---
+++ src/components/ContainerProdutos.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Card from './Card'
+import styled from 'styled-components'
+
+const DivPrincipal = styled.div`
+  `
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 15px;
+`
+
+const Grade = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15px;
+  padding: 15px;
+`
+
+export default class ContainerProdutos extends React.Component {
+  render() {
+
+    
+    return (
+
+
+      <DivPrincipal>
+        <Header>
+          <p>Quantidade de produto: 2</p>
+
+          <label>
+            Ordenação:
+            <select>
+              <option>Crescente</option>
+              <option>Descrecente</option>
+            </select>
+          </label>
+
+        </Header>
+
+        <Grade>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </Grade>
+
+      </DivPrincipal>
+    )
+  }
+}
